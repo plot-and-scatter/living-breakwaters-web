e5392cdb4f16d8e63ef5359240da9492f20e7371
Frank Hangler <frank@plotandscatter.com>
2020-04-24 08:35:05 -0700

    Better colours

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -95,23 +95,47 @@ class Map extends React.Component {
 
     console.log(LAYERS)
 
-    const checkboxes = Object.values(LAYERS).map(layer => {
-      return (
-        <div key={layer.id} className="form-check">
-          <input
-            type="checkbox"
-            id={layer.id}
-            name={layer.id}
-            value={layer.id}
-            className="form-check-input"
-            onChange={this.onCheckboxChange}
-          />
-          <label htmlFor={layer.id} className="form-check-label">
-            {layer.name}
-          </label>
-        </div>
-      )
-    })
+    const checkboxes = Object.keys(LAYERS)
+      .sort()
+      .map(key => {
+        const layer = LAYERS[key]
+        const splitColorR = parseInt(layer.color.substring(1, 3), 16)
+        const splitColorG = parseInt(layer.color.substring(3, 5), 16)
+        const splitColorB = parseInt(layer.color.substring(5), 16)
+
+        console.log(splitColorR, splitColorG, splitColorB)
+
+        const threshold = 80
+
+        const color =
+          splitColorR > threshold &&
+          splitColorG > threshold &&
+          splitColorB > threshold
+            ? "black"
+            : "white"
+
+        return (
+          <div
+            key={layer.id}
+            className="CheckboxWrapper p-1"
+            style={{ backgroundColor: layer.color, color }}
+          >
+            <div className="form-check">
+              <input
+                type="checkbox"
+                id={layer.id}
+                name={layer.id}
+                value={layer.id}
+                className="form-check-input"
+                onChange={this.onCheckboxChange}
+              />
+              <label htmlFor={layer.id} className="form-check-label">
+                {layer.name}
+              </label>
+            </div>
+          </div>
+        )
+      })
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
