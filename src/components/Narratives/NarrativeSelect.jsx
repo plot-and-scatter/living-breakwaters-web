04b56bd887e59aa80267630ddfd5e129e6b40621
Frank Hangler <frank@jupiter.telus>
2020-07-05 23:38:11 -0700

    Add changer for narrative stage

--- src/components/Narratives/NarrativeSelect.jsx
+++ src/components/Narratives/NarrativeSelect.jsx
@@ -1,13 +1,26 @@
 import React from "react"
 import { useNarrative } from "./NarrativeContext"
+import { useCallback } from "react"
 
 const NarrativeSelect = () => {
   const { narrativeStage, setNarrativeStage } = useNarrative()
+
+  const setNarrativeStageCallback = useCallback(
+    event => {
+      setNarrativeStage(event.target.value)
+    },
+    [setNarrativeStage]
+  )
 
   return (
     <div>
       <h1>Select a narrative stage</h1>
       <p>Current narrative stage: {narrativeStage}</p>
+      <input
+        type="number"
+        value={narrativeStage}
+        onChange={setNarrativeStageCallback}
+      />
     </div>
   )
 }
