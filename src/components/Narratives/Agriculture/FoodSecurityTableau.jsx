6eba9e484c92f375f917434984cf7ec4da9e095e
Frank Hangler <frank@arcturus.local>
2020-08-12 12:20:09 -0700

    Factor out SaturatedGround

--- src/components/Narratives/Agriculture/FoodSecurityTableau.jsx
+++ src/components/Narratives/Agriculture/FoodSecurityTableau.jsx
@@ -11,6 +11,7 @@ import SVGLinearGradient from "../Elements/Helpers/SVGLinearGradient"
 import BaseLayerStage from "./BaseLayers/BaseLayerStage"
 import { toggleSeaSurge } from "./BaseLayers/SeaSurge"
 import HeavyRain, { toggleRainStorm } from "./BaseLayers/HeavyRain"
+import SaturatedGround from "./BaseLayers/SaturatedGround"
 
 gsap.registerPlugin(MorphSVGPlugin)
 
@@ -135,13 +136,7 @@ const FoodSecurityTableau = () => {
           <BaseLayerStage stage={0} />
           <BaseLayerStage stage={1} />
           <BaseLayerStage stage={2} />
-          <g className="SaturatedGround" opacity={0}>
-            <path
-              id="Saturation"
-              fill="url(#_10_dpi_20_)"
-              d="M852.9 317.5H1963v-25.7l-560.5.5-36.2-.2-12.7-.9-12.8-4.2-26.7-.3-147.2 1.8-148 3.4-76.1 1.7L909 295l-14.1.7-8.9 1.7-13.4-1.6-15.6 10-4.1 11.7z"
-            />
-          </g>
+          <SaturatedGround />
         </g>
       </svg>
     </div>
