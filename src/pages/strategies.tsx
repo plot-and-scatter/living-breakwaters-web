68bf1a74dcda62b113a18aa240b8acba19a27f0c
Frank Hangler <frank@plotandscatter.com>
2020-11-27 11:09:17 -0800

    Allow strategy filtering

--- src/pages/strategies.tsx
+++ src/pages/strategies.tsx
@@ -1,11 +1,12 @@
 import { Link, graphql } from 'gatsby'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
 // import BannerRow from '../components/Rows/BannerRow'
 import Layout from '../components/Layout/Layout'
 import SEO from '../components/SEO'
 import StrategyCard from '../components/Strategies/StrategyCard'
+import { FixTypeLater } from '../components/Types/FixTypeLater'
 
 import './Strategies.scss'
 
@@ -29,22 +30,71 @@ const Strategies = (props): JSX.Element => {
 
   const strategies = data.allMarkdownRemark.edges
 
+  const [filters, setFilters] = useState<FixTypeLater[]>([...strategyTypes])
+
+  const filteredStrategies = strategies.filter((strategyNode) =>
+    filters.includes(strategyNode.node.frontmatter.strategyTypes)
+  )
+
+  const checkboxCallback = useCallback(
+    (event) => {
+      const newFilters = [...filters]
+      const value = event.target.value
+      if (event.target.checked) {
+        newFilters.push(value)
+      } else {
+        const indexOfItem = newFilters.indexOf(value)
+        newFilters.splice(indexOfItem, 1)
+      }
+      setFilters(newFilters)
+    },
+    [filters]
+  )
+
+  useEffect(() => {
+    console.log(filters)
+  }, [filters])
+
   const siteTitle = data.site.siteMetadata.title
   return (
     <Layout location={props.location} title={siteTitle}>
       <SEO title="Strategies" />
-      {/* <BannerRow
-        bgColorRGB={'rgb(255, 255, 255)'}
-        bgOpacity={0.0}
-        bgImage={image}
-        textDark
-      >
-        Strategies
-      </BannerRow> */}
-      <h1>Strategies</h1>
-
+      <div className="row my-4 align-items-center">
+        <div className="col-6">
+          <h1 className="mb-0">Strategies</h1>
+        </div>
+        <div className="col-6 text-right">
+          <div className="d-flex justify-content-end align-items-center">
+            <h5 className="text-muted mr-3 mb-0">Show strategies:</h5>
+            {strategyTypes.map((strategyType) => {
+              const color = colorForStrategy(strategyType)
+              return (
+                <div
+                  key={strategyType}
+                  className={`py-1 px-2 form-check form-check-inline border border-${color} text-${color}`}
+                >
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    id={`InlineCheckbox-${strategyType}`}
+                    value={strategyType}
+                    onChange={checkboxCallback}
+                    defaultChecked={filters.includes(strategyType)}
+                  />
+                  <label
+                    className="form-check-label"
+                    htmlFor={`InlineCheckbox-${strategyType}`}
+                  >
+                    {strategyType}
+                  </label>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
       <div className="StrategyCards card-columns">
-        {strategies.map((strategyNode, index) => {
+        {filteredStrategies.map((strategyNode, index) => {
           const strategy = strategyNode.node
           return <StrategyCard key={index} strategy={strategy} />
         })}
