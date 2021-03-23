cdcae46100d1a960d1a21efe256678dc6e5d7ecf
Frank Hangler <frank@plotandscatter.com>
2021-03-23 10:39:11 -0700

    Sea level for waste

--- src/pages/narratives.tsx
+++ src/pages/narratives.tsx
@@ -15,7 +15,7 @@ const Narratives = (props: FixTypeLater): JSX.Element => {
   const siteTitle = data.site.siteMetadata.title
 
   const [activeNarrative, setActiveNarrative] = useState(
-    NarrativeType.WasteElectricity
+    NarrativeType.CommunityVulnerability
   )
 
   return (
