64e256945554999c30064d81119b6bbd82989e71
Frank Hangler <frank@plotandscatter.com>
2021-06-24 14:09:38 -0700

    Masking for plants

--- src/components/Narratives/FoodSecurity/Popovers/IntertidalPop.tsx
+++ src/components/Narratives/FoodSecurity/Popovers/IntertidalPop.tsx
@@ -21,15 +21,22 @@ const IntertidalPop = (props: PlaceableSVGProps): JSX.Element => {
   const extraClasses = narrativeStage >= 1 ? 'Red' : ''
 
   return (
-    <svg className={`Popover ${extraClasses}`} onClick={onClickText}>
-      <IntertidalPlant xOffset={0.1 + 0.0} yOffset={0.8 - 0.0} />
-      <IntertidalPlant xOffset={0.1 + 0.04} yOffset={0.8 - 0.01} />
-      <IntertidalPlant xOffset={0.1 + 0.08} yOffset={0.8 - 0.02} />
-      <IntertidalPlant xOffset={0.1 + 0.12} yOffset={0.8 - 0.03} />
-      <Textbox xOffset={0.15} yOffset={0.85} textboxWidth={90}>
-        Intertidal
-      </Textbox>
-    </svg>
+    <>
+      <svg className={`Popover ${extraClasses}`} onClick={onClickText}>
+        <IntertidalPlant xOffset={0.1 + 0.006} yOffset={0.8 - 0.01} />
+        <IntertidalPlant xOffset={0.1 + 0.04} yOffset={0.8 - 0.02} />
+        <IntertidalPlant xOffset={0.1 + 0.075} yOffset={0.8 - 0.025} />
+        <IntertidalPlant xOffset={0.1 + 0.115} yOffset={0.8 - 0.03} />
+        <Textbox xOffset={0.15} yOffset={0.85} textboxWidth={90}>
+          Intertidal
+        </Textbox>
+      </svg>
+      <path
+        className="GroundColor"
+        // fillOpacity={0.5}
+        d="M84 256 l 45 -5 l 90 -4 v 15 h -135"
+      />
+    </>
   )
 }
 
