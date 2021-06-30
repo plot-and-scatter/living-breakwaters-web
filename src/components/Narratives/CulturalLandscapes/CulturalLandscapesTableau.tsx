e59f4a1ac219eaaf9a771cd04ecc1a7c2b5d45d2
Frank Hangler <frank@plotandscatter.com>
2021-06-30 15:04:03 -0700

    Tweaks

--- src/components/Narratives/CulturalLandscapes/CulturalLandscapesTableau.tsx
+++ src/components/Narratives/CulturalLandscapes/CulturalLandscapesTableau.tsx
@@ -45,53 +45,56 @@ const CulturalLandscapesTableau = ({ setFrameContent }: Props): JSX.Element => {
     }
   }, [narrativeStage])
 
+  // TODO: Add dashed line for sea level
+
+  // TODO: With rain event, animate the washing away of the intertidal
+  // area into the water
+
+  // TODO: Maybe with rain event, particles come from the bus / transit area too
+
+  // TODO: With sea level rise, animate up the dashed line to match the new
+  // sea level (or show another sea level); show there are changes to the
+  // groundwater table in the park area; this would be different from the
+  // contamination hatch. We want to show the saturated zone moving up.
+
+  // TODO: Change the hatching for the various items: contamination vs. saltwater
+  // vs. groundwater
+
+  // TODO: Don't completely flood the subway
+
   return (
     <div className="CulturalLandscapesTableau">
       <SVGFrame id="CulturalLandscapesTableau">
         <HeavyRain xOffset={0} yOffset={0.35} />
-
         <Retail xOffset={0.84} yOffset={0.48} />
-
         <Basement xOffset={0.7} yOffset={0.83} scale={0.17} />
 
         <CulturalLandscapesStormSurge />
         <CulturalLandscapesSeaLevelRise />
         <CulturalLandscapesSeaLevel />
         <CulturalLandscapesGround stage={narrativeStage} yOffset={0.8} />
-
         <Birds xOffset={0.2} yOffset={0.3} />
-
         <FishingPop onClick={setFrameContent} />
         <IntertidalPop onClick={setFrameContent} />
         <BeachPop onClick={setFrameContent} />
-
         <BikePathPop onClick={setFrameContent} />
         <ArchaeologyPop onClick={setFrameContent} />
         <ParkPop onClick={setFrameContent} />
-
         <TransitPop onClick={setFrameContent} />
-
         <HotelPop onClick={setFrameContent} />
-
         <Office3 xOffset={0.72} yOffset={0.59} />
-
         <SubwayPop onClick={setFrameContent} />
-
         {/* <Tree xOffset={0.425} yOffset={0.7} scale={0.04} /> */}
         {/* <Tree xOffset={0.425 + 0.03} yOffset={0.7} scale={0.04} /> */}
         {/* <Tree xOffset={0.58} yOffset={0.685} scale={0.04} /> */}
         {/* <Tree xOffset={0.58 + 0.03} yOffset={0.685} scale={0.04} /> */}
         <TrafficLight scale={0.03} xOffset={0.53} yOffset={0.672} />
-
         {/* <Tree xOffset={0.69} yOffset={0.685} scale={0.04} /> */}
         <Tree xOffset={0.585} yOffset={0.688} scale={0.04} />
         {/* <House1 scale={0.06} xOffset={0.725} yOffset={0.685} /> */}
         {/* <House1 scale={0.06} xOffset={0.82} yOffset={0.67} /> */}
-
         {/* <path className="FillGround" d="M655 233 l 50 -4.5 v 5 h -50" /> */}
-
         {/* <House1 scale={0.06} xOffset={0.905} yOffset={0.647} /> */}
-
         <Tree xOffset={0.775} yOffset={0.687} scale={0.04} />
         <Tree xOffset={0.775 + 0.03} yOffset={0.687} scale={0.04} />
         <Midrise xOffset={0.942} yOffset={0.656} />
--- src/components/Narratives/CulturalLandscapes/Popovers/ArchaeologyPop.tsx
+++ src/components/Narratives/CulturalLandscapes/Popovers/ArchaeologyPop.tsx
@@ -18,11 +18,13 @@ const BasementPop = (props: PlaceableSVGProps): JSX.Element => {
     }
   }, [narrativeStage, onClick])
 
+  // TODO: Put the Midden underground
   const extraClasses = narrativeStage >= 2 ? 'Red' : ''
 
   return (
     <svg className={`Popover ${extraClasses}`} onClick={onClickText}>
-      <Midden scale={0.08} xOffset={0.35} yOffset={0.758} />
+      <Midden scale={0.1} xOffset={0.33} yOffset={0.77} />
+      <Midden scale={0.11} xOffset={0.34} yOffset={0.79} />
       <Textbox xOffset={0.39} yOffset={0.93} textboxWidth={110}>
         Archaeology
       </Textbox>
