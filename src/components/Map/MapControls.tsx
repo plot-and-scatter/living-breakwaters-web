9b149231eec14c1bc8b077efbd4491afd3d95645
Frank Hangler <frank@plotandscatter.com>
2021-08-03 13:36:15 -0700

    Change base layer on map. Fix #69

--- src/components/Map/MapControls.tsx
+++ src/components/Map/MapControls.tsx
@@ -1,12 +1,35 @@
 import React, { useCallback } from 'react'
 
-import { MAP_CENTRE, START_ZOOM } from './mapHelper'
+import { MAP_CENTRE, MAP_STYLE, START_ZOOM } from './mapHelper'
 import { useMapManager } from '../Data/MapLayerManager'
 
 import './MapControls.scss'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+const baseLayerStyles = [
+  MAP_STYLE,
+  'mapbox://styles/mapbox/light-v10',
+  'mapbox://styles/mapbox/dark-v10',
+  'mapbox://styles/mapbox/streets-v11',
+  'mapbox://styles/mapbox/outdoors-v11'
+]
 
 const MapControls = (): JSX.Element => {
   const { map } = useMapManager()
+
+  const [baseLayerIndex, setBaseLayerIndex] = useState<number>(0)
+
+  useEffect(() => {
+    console.log('=========> IN HERE')
+    if (baseLayerIndex >= baseLayerStyles.length) {
+      setBaseLayerIndex(0)
+    } else {
+      if (map) {
+        map.setStyle(baseLayerStyles[baseLayerIndex])
+      }
+    }
+  }, [baseLayerIndex, map])
 
   const onZoomInClick = useCallback(() => {
     map.zoomIn()
@@ -25,18 +48,35 @@ const MapControls = (): JSX.Element => {
     })
   }, [map])
 
+  const onBaseLayerChangeClick = useCallback(() => {
+    setBaseLayerIndex(baseLayerIndex + 1)
+  }, [map, baseLayerIndex])
+
   return (
-    <div className="MapControls btn-group">
-      <button className="btn btn-sm btn-outline-dark" onClick={onZoomInClick}>
-        <i className="fas fa-plus" />
-      </button>
-      <button className="btn btn-sm btn-outline-dark" onClick={onZoomOutClick}>
-        <i className="fas fa-minus" />
-      </button>
-      <button className="btn btn-sm btn-outline-dark" onClick={onResetClick}>
-        <i className="far fa-compass mr-2" />
-        Reset
-      </button>
+    <div className="MapControls d-flex justify-content-around">
+      <div className="btn-group">
+        <button className="btn btn-sm btn-outline-dark" onClick={onZoomInClick}>
+          <i className="fas fa-plus" />
+        </button>
+        <button
+          className="btn btn-sm btn-outline-dark"
+          onClick={onZoomOutClick}
+        >
+          <i className="fas fa-minus" />
+        </button>
+        <button className="btn btn-sm btn-outline-dark" onClick={onResetClick}>
+          <i className="far fa-compass mr-1" />
+          Reset
+        </button>
+      </div>
+      <div className="btn-group">
+        <button
+          className="btn btn-sm btn-outline-dark"
+          onClick={onBaseLayerChangeClick}
+        >
+          <i className="fas fa-layer-group mr-1" /> Base
+        </button>
+      </div>
     </div>
   )
 }
