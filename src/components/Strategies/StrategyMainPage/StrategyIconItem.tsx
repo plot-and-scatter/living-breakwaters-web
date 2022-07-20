c553821ba75d2d7979afaf148a81e176cc2ae731
Frank Hangler <frank@plotandscatter.com>
2022-07-20 12:24:58 -0700

    Baseline icons

--- src/components/Strategies/StrategyMainPage/StrategyIconItem.tsx
+++ src/components/Strategies/StrategyMainPage/StrategyIconItem.tsx
@@ -11,18 +11,29 @@ interface Props {
 
 const StrategyIconItem = ({ strategy }: Props): JSX.Element => {
   const strategyType = strategy.node.frontmatter.strategyTypes
-  return (
-    <div className={`col-3 StrategyIconItem mb-4`}>
-      <Link to={strategy.node.fields.slug}>
-        <div className={`${strategyType}BG p-2 h-100`}>
-          <p>
-            <small>{strategy.node.frontmatter.title}</small>
-          </p>
-          <div className="text-center">
-            <i className="fas fa-island-tropical fa-4x" />
-          </div>
 
-          <i className="fas fa-arrow-right hidden" />
+  const slug = strategy?.node?.fields?.slug
+
+  if (!slug) return <></>
+
+  const slugWithoutTrailingSlash = slug.substring(0, slug.length - 1)
+
+  const iconPath = `${slugWithoutTrailingSlash}.svg`
+
+  return (
+    <div className={`col-6 col-sm-4 StrategyIconItem mb-4`}>
+      <Link to={slug}>
+        <div className={`${strategyType}Item p-2 h-100 d-flex `}>
+          <div className="Title">
+            <p>{strategy.node.frontmatter.title}</p>
+          </div>
+          <div className="text-center">
+            <img src={iconPath} />
+          </div>
+          <div className="text-center hidden Action">
+            Learn more&nbsp;
+            <i className="fas fa-arrow-right mt-2" />
+          </div>
         </div>
       </Link>
     </div>
